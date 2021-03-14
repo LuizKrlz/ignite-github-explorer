b02e800f67684b01de4b1c8e26ba6daae31b1dee
Luiz Correa <luiz.correa.dev@gmail.com>
2021-03-14 15:45:28 -0300

    configurate typescript

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import { RepositoryItem } from "./RepositoryItem";
-import "../styles/repositories.scss";
+import { useEffect, useState } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.github.com/orgs/rocketseat/repos")
+    fetch('https://api.github.com/orgs/rocketseat/repos')
       .then((response) => response.json())
       .then((data) => {
         setRepositories(data);
